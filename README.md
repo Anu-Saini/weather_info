7d119b4ad4ef35f778cc6e2fdd4ae4152d413ff5
Anu.Saini <105421965+Anu-Saini@users.noreply.github.com>
2022-07-10 20:46:13 +1000

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # weather_info
+# Weather_app
+The application has been developed to help users know about the weather at a particular city. TYhe knowledge of having parameters of weather may help them plan their journey to the destination or understanding the severity of the climate at that place for social, or economic reasons. The current application uses data from openweather api and utilizes the javascript, bootstrap, moments js and other application to extract the data and display information dynamically over a period of 5 days.The program access their data and functionality by making requests with specific parameters to a URL.The api used for this application is from OpenWeather API to retrieve weather data for cities.
+
+## User Story
+AS A traveler I am able to see the weather pattern for multiple cities before travelling so that i can arrange my visits , business meetings or holiday plans accordingly.
+
+I am able to type in a city name and the application return me temperature, humidity, wind for that particular place. It not only gives me the weather particular for that particular date, but for the next five consecutive days as well.
+
+I am able to get a list of last 5 cities i have searched and I can revisit to any of those cities to have their weather data displayed again and if a city that i am visiting has a high index of UV radiation, it is highlighted by a colour code for easy view..
+
+# User page
+1. as a user when i open the webpage, I am looking at the weather App with easy functionality.  can easily see a search bar to the left for city search.
+![img1](https://user-images.githubusercontent.com/105421965/178138450-242cb147-07c7-4061-ae86-31f875c8052b.jpg)
+
+2. as a user, whn i put in a particular city that i am interested in, the app shows me the current weather condition as of now ie today with some weather particulars like, temperature, temperature feels like, Sky conditions, wind speed, humiidty and UV index. It also shows UnV Index as colour coded. 
+As a user it also presents me the weather update for the next 5 day and also shows the particular city i searched in the history on the left pannel.
+![img2](https://user-images.githubusercontent.com/105421965/178138457-90688fc9-0c7f-4bef-8851-52c01349797d.jpg)
+
+3. As a user i am able to search for another city, and the webpage will show me weather particlar fopr the new city along with this city also in history.
+ ![img3](https://user-images.githubusercontent.com/105421965/178138467-469c855f-7471-4a32-8a21-0eb7d267de87.jpg)
+
+
+ 4. As a user, the webpage indicate me a warning about high UV index as it being color coded to red. with index being shown 
+ ![img4](https://user-images.githubusercontent.com/105421965/178138476-2c6f8574-54b4-4e98-8347-5949264da262.jpg)
+
+
+5. As a user, i am able to revisit the weather details of any particular city that i have looked uop earlier, easily by clicking the city name in history.
+even the city which has relatiuve low UV index is shown in green for easy reference. High UV Index is highlighted by red colour and moderate by brown/pink colour.
+![img5](https://user-images.githubusercontent.com/105421965/178138489-81668336-7159-452c-b369-5d2aa23f0cab.jpg)
+![img6](https://user-images.githubusercontent.com/105421965/178138497-e9668235-cf53-4717-b11e-29cee44fc051.jpg)
+
+
+Reference-
+Openweather API for the data 
+
+
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,176 @@
+
+// assigning var names to to Ids
+var cityFormEl = document.querySelector('#city-form');
+var nameInputEl = document.querySelector('#cityname');
+var cityContainerEl = document.querySelector('#city-container');
+var citySearchTerm = document.querySelector('#city-search-term');
+var locationCity = document.querySelector('#maincard');
+var a1 = document.querySelector('#card1');
+//assigning var name to an empty array
+var box = [];
+
+// create elements tags for updating webpage dynamically
+let list = document.createElement('ul');
+var temp1 = document.createElement('li');
+var temp_now = document.createElement('li');
+let cloudy = document.createElement('li');
+var wind1 = document.createElement('li');
+let pressure1 = document.createElement('li');
+let moist = document.createElement('li');
+let head = document.createElement('h6')
+
+//for city list counter
+var cityList = document.querySelector('#city-list')
+
+//creating search list for cities entered
+var cities = [];
+function renderCity() {
+  cityList.innerHTML = "";
+  const length = cities.length > 5 ? 5 : cities.length;
+
+  for (var i = 0; i < length; i++) {
+    var li = document.createElement("li");
+    li.setAttribute('class', 'recentcities');
+    let a = document.createElement("a");
+    a.setAttribute("data-index", i);
+    a.onclick = function (event) {
+      const id = event.target.getAttribute('data-index');
+      inputCity(cities[id]);
+    };
+    a.textContent = cities[i];
+    li.setAttribute("data-index", i);
+    li.appendChild(a);
+    cityList.appendChild(li);
+  }
+};
+// creating a function initiate by submit button on the city search
+function iCity(event) {
+  event.preventDefault();
+
+  var city = nameInputEl.value.trim();
+  if (city) {
+    cityContainerEl.textContent = '';
+    nameInputEl.value = '';
+    cities.push(city);
+    inputCity(city);
+
+  } else {
+    alert('Please enter a City name');
+  }
+}
+
+
+function inputCity(city) {
+  var requestedUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=9b1192177659da66737e8df6341c92d7&units=metric'
+
+  fetch(requestedUrl)
+    .then(function (response) {
+       if (response.ok) {
+      return response.json();
+    }})
+    .then(function (data) {
+   
+      //   var requestedUrl ='https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=a9ccb03a3d3005daee42faa0a3e713c1&units=metric'
+      var requestedUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +data[0].lat + '&lon=' +data[0].lon + '&exclude=hourly&appid=a9ccb03a3d3005daee42faa0a3e713c1&units=metric'
+      fetch(requestedUrl)
+        .then(function (response) {
+          if (response.ok) {
+            return response.json();
+          }})
+     .then(function (data) {
+            box = (data);
+            console.log(data);
+            console.log(box.current.weather.description);
+
+            //assigning current date to a variable
+            var today = moment().format("DD/MM/YYYY");
+
+            //assigning various values to parameters at index 0 of the array from the api cal
+            temp1.innerText = `Temp: ${box.current.temp} ℃`;
+            temp_now.innerText = `Temp(feel like): ${box.current.feels_like} ℃`;
+            cloudy.innerText = `sky: ${box.current.weather[0].description} `;
+            wind1.innerText = `Wind: ${box.current.wind_speed} km/hr`;
+            moist.innerText = `Humidity: ${box.current.humidity} g/kg`;
+            pressure1.innerText = `UV Index: ${box.current.uvi}`;
+            var UVI=box.current.uvi
+
+            head.innerText = `City : ${city}  ( on ${today} )`;
+            //head.innerText = `City : ${box.city.name}  ( on ${today} )`;
+            //appending the values to the elements created dynamically
+            locationCity.append(head);
+            console.log(locationCity)
+            list.append(temp1);
+            list.append(temp_now);
+            list.append(cloudy);
+            list.append(wind1);
+            list.append(moist);
+            list.append(pressure1);
+            locationCity.append(list);
+            
+if (UVI<=4 ){
+  pressure1.setAttribute('class','normal')
+} 
+else if (UVI>4 && UVI<=7) 
+{
+  pressure1.setAttribute('class','moderate')
+} 
+else 
+{
+  pressure1.setAttribute('class','severe')
+};
+
+            //creating the iteation function for xtracting the value of the array over next 5 days.
+            for (i = 1; i <= 5; i++) {
+              let list_ = document.createElement('ul');
+              var temp1_ = document.createElement('li');
+
+              var temp_max_ = document.createElement('li');
+              let cloudy_ = document.createElement('li');
+              var wind1_ = document.createElement('li');
+              let pressure1_ = document.createElement('li');
+              let moist_ = document.createElement('li');
+              let date = document.createElement('h6');
+              list_.setAttribute('class', 'nextdays');
+
+              temp1_.innerText = `Temp: ${box.daily[i].temp.day} ℃`;
+              temp_max_.innerText = `Temp(feels): ${box.daily[i].feels_like.day} ℃`;
+              cloudy_.innerText = `sky: ${box.daily[i].weather[0].description} `;
+              wind1_.innerText = `Wind: ${box.daily[i].wind_speed} km/hr`;
+              moist_.innerTextt = `Humidity: ${box.daily[i].humidity} g/kg`;
+              pressure1_.innerText = `UV Index: ${box.daily[i].uvi} `;
+
+              var duedate = moment().add(i, 'days').format("DD/MM/YYYY");
+              date.innerText = `${duedate}`;
+
+              console.log(duedate);
+
+              let id = `day${i}`;
+
+              var customref = document.getElementById(id)
+              let head_ = document.createElement('h3')
+
+              //appending in dynamically created elements
+              list_.append(date);
+              list_.append(temp1_);
+              list_.append(temp_max_);
+              list_.append(cloudy_);
+              list_.append(wind1_);
+              list_.append(moist_);
+              list_.append(pressure1_);
+              const prevChild = customref.getElementsByTagName('ul');
+              if (prevChild.length > 0)
+                customref.removeChild(customref.getElementsByTagName('ul')[0]);
+              customref.append(list_);
+              customref.append(head_);
+            }
+          });
+          renderCity()
+        });
+      };
+      //calling the function on a event of submitting the city search form.
+      cityFormEl.addEventListener('submit', iCity);
+
+    
+//formSubmitHandler
+
+
